e359844180ac20023c1217b9596c09ad9f4b1927
mkessler <mkessler@student.ethz.ch>
2023-12-13 17:46:05 +0100

    generated json files

--- part0_test.py
+++ part0_test.py
@@ -1,13 +1,8 @@
-"""
-PCL 1 Fall Semester 2023 - Course Project
-Part 0: Book Selection
-Students: Mattia Turra, Mariela Kessler
-"""
-
-# --- Imports ---
 import os
 import re
 import json
+
+
 # --- Don't add other imports here ---
 
 
@@ -21,12 +16,12 @@ def json_conversion(data):
 
     print("Data before processing: ", data)
     for content in data:
+
         words = content.strip().split(',')
-        result_data.extend([{'PROPN': word} for word in words])
+        result_data.extend([{'ADJ': word} for word in words])
         result_data.pop()  # remove the last element of the list because it's empty
     print("Data after processing: ", result_data)
     return json.dumps(result_data)
-
 
 def write_as_json(data, file_path):
     """
@@ -50,15 +45,16 @@ def create_entities_sentiment_folders(book_path):
         if not os.path.exists(sentiment_folder):
             os.makedirs(sentiment_folder)
 
-        # sentiment_folder = None # to avoid UnboundLocalError
+        #sentiment_folder = None # to avoid UnboundLocalError
 
     elif book_path.endswith('entities'):
         entities_folder = os.path.join(book_path.replace('entities', 'entities_json'))
         if not os.path.exists(entities_folder):
             os.makedirs(entities_folder)
-        # entities_folder = None # to avoid UnboundLocalError
+        #entities_folder = None # to avoid UnboundLocalError
 
     return entities_folder, sentiment_folder
+
 
 def main():
     # Here you may add the neccessary code to call your functions, and all the steps before, in between, and after calling them.
@@ -75,13 +71,14 @@ def main():
                 data = infile.readlines()
 
             data_type = 'entities' if 'entities' in book_path else 'sentiments'
-            # json_data = json_conversion(data)
+            #json_data = json_conversion(data)
             output_folder = entities_folder if data_type == 'entities' else sentiment_folder
             output_path = os.path.join(output_folder, f"{file_name.replace('.txt', '.json')}")
             output_path = os.path.join(output_folder, f"{file_name.replace('.txt', '.json')}")
 
             json_data = json_conversion(data)
             write_as_json(json_data, output_path)
+
 
 # This is the standard boilerplate that calls the main() function when the program is executed.
 if __name__ == '__main__':
